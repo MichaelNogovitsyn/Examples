2646d25abd22ed3fea19ba42bf65e039e1fad6b7
mexatron <mexatron>
2022-11-10 20:02:26 +0200

    add random number

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int nubmberB = 5;
-int result = numberA+nubmberB;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA+numberB;
 Console.WriteLine(result);
